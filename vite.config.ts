c32e4d7dad2081d486546d8fb3fec741592d9791
pontush81 <149173481+pontush81@users.noreply.github.com>
2025-03-03 16:27:46 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Viktigt för att Vercel ska hitta rätt filer
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  // Add this to suppress the findDOMNode warning in development
   esbuild: {
     logOverride: { 'this-is-undefined-in-esm': 'silent' }
   }
